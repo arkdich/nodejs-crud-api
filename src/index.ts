ac78c7a8c1e82c8da0f3e0a0e1604322e9506246
Artemy Dyachuk <artem002498@gmail.com>
2024-02-06 00:04:47 +0600

    add: basic project & server config

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+import { createReadStream } from 'fs'
+import { createServer, request } from 'http'
+
+const PORT = process.env.SERVER_PORT
+
+const server = createServer((req, res) => {
+  console.log(`Incoming ${req.method} request`)
+
+  const readStream = createReadStream('src/static/text.txt')
+  readStream.pipe(res)
+
+  res.writeHead(200, 'OK', {
+    'content-type': 'plain/text',
+  })
+
+  readStream.on('end', () => {
+    res.end()
+  })
+})
+
+server.on('listening', () => {
+  console.log(
+    `Server is listening on port ${PORT}\nAccess on http://localhost:${PORT}`
+  )
+
+  const clientRequest = request({
+    method: 'POST',
+    protocol: 'http:',
+    origin: 'localhost',
+    port: PORT,
+  })
+
+  clientRequest.end('fdfodfk')
+})
+
+server.listen(PORT)
