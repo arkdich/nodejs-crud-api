b068886c4c7b8c0f0203c23d06a951d97d5f7274
Artemy Dyachuk <artem002498@gmail.com>
2024-02-10 21:38:35 +0600

    chore: rm test func

--- src/index.ts
+++ src/index.ts
@@ -17,36 +17,4 @@ const server = createServer((req, res) => {
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server running at http://${HOSTNAME}:${PORT}`)
-
-  const clientRequest = request({
-    method: 'GET',
-    protocol: 'http:',
-    host: 'localhost',
-    path: '/api/users/42',
-    port: PORT,
-  }).end()
-
-  clientRequest.on('response', (res) => {
-    console.log(`\n--------\nResponse: ${res.statusCode} ${res.statusMessage}`)
-
-    res.setEncoding('utf-8').on('data', (chunk) => {
-      process.stdout.write('Body: ')
-      console.log(chunk)
-    })
-  })
 })
-
-process.stdin.on('data', (chunk) => {
-  const input = chunk.toString().trim()
-
-  if (input === 'list') {
-    process.stdout.write('users: ')
-    console.dir(userDb.getAll())
-  }
-
-  if (input === 'clear') {
-    console.clear()
-  }
-})
-
-// curl -X POST http://localhost:3000/api/users -d '{"username":"Artemy","age":25,"hobbies":[]}'
